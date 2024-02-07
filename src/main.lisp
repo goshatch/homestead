57f1216bcca880d70c808ff7690e893d33211531
Gosha Tcherednitchenko <git@gosha.net>
2024-02-07 22:30:35 +0000

    feat(metadata): Traverse metadata tree

--- src/main.lisp
+++ src/main.lisp
@@ -2,21 +2,42 @@
   (:use :cl))
 (in-package :homestead)
 
-(defun separate-content-and-frontmatter (file-path)
-  (with-open-file (stream file-path)
-    (let* ((line (read-line stream nil nil))
-            (in-frontmatter (string= line "---"))
-            frontmatter
-            content)
-      (setf frontmatter (with-output-to-string (s)
-                          (when in-frontmatter
-                            (loop while in-frontmatter do
-                              (setf line (read-line stream nil nil))
-                              (if (string= line "---")
-                                (setf in-frontmatter nil)
-                                (write-line line s))))))
-      (setf content (with-output-to-string (s)
-                      (loop with end-of-file = nil
-                        while (setf line (read-line stream nil end-of-file))
-                        do (write-line line s))))
-      (list frontmatter content))))
+;; Example of metadata tree
+;; '(("/"
+;;     (:title "Home")
+;;     ("projects"
+;;       (:title "Projects"
+;;         :rss t)
+;;       ("astrovox"
+;;         (:title "Astrovox"
+;;           :date "2024-01-01"
+;;           :keywords '("software" "synthesizer")))
+;;       ("where-the-story-begins"
+;;         (:title "Where the Story Begins"
+;;           :date "2019-05-05"
+;;           :keywords '("book design" "photography"))))
+;;     ("blog"
+;;       (:title "Blog"
+;;         :rss t))
+;;     ("about"
+;;       (:title "About"))))
+
+(defun process-metadata-tree (tree &optional parent-permalink)
+  (when tree
+    (let* ((node (car tree))
+           (permalink (car node))
+           (attributes (cadr node))
+           (children (list (cddr node)))
+           (full-permalink (if parent-permalink
+                               (concatenate 'string
+                                 parent-permalink
+                                 (if (string= parent-permalink "/")
+                                   permalink
+                                   (concatenate 'string "/" permalink)))
+                               permalink)))
+      (format t "Processing page: \"~a\" [~a]~%" (getf attributes :title) full-permalink)
+      (dolist (child children)
+        (process-metadata-tree child full-permalink)))
+    (let ((siblings (cdr tree)))
+      (when siblings
+        (process-metadata-tree siblings parent-permalink)))))
