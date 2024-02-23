beba65d71fd57b14f6c35468025951303d834734
Gosha Tcherednitchenko <git@gosha.net>
2024-02-23 12:58:12 +0000

    feat(util): file finder functions

--- src/util.lisp
+++ src/util.lisp
@@ -1,7 +1,12 @@
 (defpackage homestead/util
   (:nicknames #:util)
   (:use #:cl)
-  (:export #:join #:slurp))
+  (:export
+    #:join
+    #:slurp
+    #:build-path
+    #:find-file-path
+    #:find-include-path))
 
 (in-package :homestead/util)
 
@@ -18,3 +23,20 @@
       (loop for line = (read-line stream nil nil)
         while line do (write-string line out)
         (write-char #\Newline out)))))
+
+(defun build-path (permalink &optional (extension "html"))
+  "Return path for file with extension EXTENSION representing PERMALINK"
+  (concatenate 'string (homestead:get-setting "site-root") "/" permalink "." extension))
+
+(defun find-file-path (permalink)
+  "Find the file with the appropriate extension for PERMALINK"
+  (let ((possible-paths
+          (mapcar
+            (lambda (ext) (build-path permalink ext))
+            (homestead:get-setting "allowed-extensions"))))
+    (find-if (lambda (path) (probe-file path)) possible-paths)))
+
+(defun find-include-path (name)
+  "Find the file with the appropriate extension for the include with NAME"
+  (find-file-path
+    (concatenate 'string "_includes/" name)))
